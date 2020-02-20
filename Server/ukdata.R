dc5e87411802d8bc6f040010e2745296f5f34e44
piabenaud <P.Benaud@exeter.ac.uk>
2020-02-20 08:52:53 +0000

    Updates to table display

--- Server/ukdata.R
+++ Server/ukdata.R
@@ -4,9 +4,19 @@
 output$erosiontable <- DT::renderDataTable({
   DT::datatable(cleantable, 
                 escape = FALSE,
-                plugins = "ellipsis", 
+                plugins = "ellipsis",
+                extensions = c('Buttons','FixedColumns','FixedHeader', 'Scroller'),
                 options = list(
-                  pageLength = 25,
+                  #pageLength = 25,
+                  orderClasses = TRUE,
+                  dom = 'Bfrtlip',
+                  buttons = 'csv',
+                  scrollX = TRUE,
+                  fixedColumns = list(leftColumns = 4),
+                  fixedHeader = TRUE,
+                  deferRender = TRUE,
+                  scrollY = 500,
+                  scroller = TRUE,
                   columnDefs = list(list(
                     targets = 20,
                     render = JS("$.fn.dataTable.render.ellipsis(20, false)")
