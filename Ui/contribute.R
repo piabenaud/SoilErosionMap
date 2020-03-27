c0bf39d72cdbe9b89e2c358dbf7d75ba1088dbf3
piabenaud <P.Benaud@exeter.ac.uk>
2020-03-27 13:10:40 +0000

    Updates to categories and notes

--- Ui/contribute.R
+++ Ui/contribute.R
@@ -6,7 +6,7 @@
 fluidPage(
   titlePanel("Contribute soil erosion data to global map"),
   p("Please use the add data function to add additional rows to the table. Once you have finished, download the data and email it to", a(href = 'http://geography.exeter.ac.uk/staff/index.php?web_id=Pia_Benaud', 'Pia Benaud'), "or submit it as a pull-request on the", a(href = 'https://github.com/piabenaud/SoilErosionMap', 'SoilErosionMap'), "github page."),
-  p("All data will be checked for consistency before being added to the global geodatabase and map."),
+  p("All data will be checked for consistency before being added to the global geodatabase and map. Please note, contributions are currently be limited to spatially explicit observations of on-site erosion - records based on suspended sediment yields and bathymetry (i.e. off-site representations of soil erosion) will not be included."),
   hr(),
   downloadButton("Erosion_csv", "Download as CSV"),
   br(),
